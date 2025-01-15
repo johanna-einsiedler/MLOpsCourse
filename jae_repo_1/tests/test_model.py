0d6c41090c64ac2efd220e56ef47bfc7269ae630
johanna-einsiedler <j.eremita@gmx.at>
2025-01-15 18:45:48 +0100

    removed wrong test

---
+++ jae_repo_1/tests/test_model.py
@@ -0,0 +1,11 @@
+from test_project.model import MyAwesomeModel
+import torch
+import pytest
+
+@pytest.mark.parametrize("batch_size", [32, 64])
+def test_model(batch_size: int):
+    model = MyAwesomeModel()
+    x = torch.randn(batch_size, 1, 28, 28)
+    y = model(x)
+    assert y.shape == (batch_size, 10)
+
